8cef7fe5577b7d1a7619f2c10439f13de0964cef
jari.tenhunen <jari.tenhunen@outlook.com>
2025-11-24 09:07:10 +0100

    Initial commit

---
+++ src/test/java/com/statistics/statisticNonsense/StatisticNonsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statistics.statisticNonsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticNonsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
