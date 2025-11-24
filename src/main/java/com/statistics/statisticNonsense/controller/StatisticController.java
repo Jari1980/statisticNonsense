4f67e480b4a9eb90de264524d3eb9dbf9b8450ed
jari.tenhunen <jari.tenhunen@outlook.com>
2025-11-24 13:45:35 +0100

    Crud operations for products added, changed hibernate state to update inorder to persist items in MySQL

--- src/main/java/com/statistics/statisticNonsense/controller/StatisticController.java
+++ src/main/java/com/statistics/statisticNonsense/controller/StatisticController.java
@@ -1,7 +1,10 @@
 package com.statistics.statisticNonsense.controller;
 
+import com.statistics.statisticNonsense.entity.Product;
 import com.statistics.statisticNonsense.model.ButtonLog;
 import com.statistics.statisticNonsense.service.ButtonService;
+import com.statistics.statisticNonsense.service.ProductService;
+import jakarta.transaction.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,11 +14,14 @@ import java.util.List;
 public class StatisticController {
 
     private final ButtonService buttonService;
+    private final ProductService productService;
 
-    public StatisticController(ButtonService buttonService){
+    public StatisticController(ButtonService buttonService, ProductService productService){
         this.buttonService = buttonService;
+        this.productService = productService;
     }
 
+    //Requests for buttons logging and reading data to/from csv file
     @PostMapping("/button/A")
     public ButtonLog buttona(@RequestParam String value){
         return buttonService.registerClick("A", value);
@@ -29,5 +35,31 @@ public class StatisticController {
     @GetMapping("/clicks")
     public List<ButtonLog> getLogs() {
         return buttonService.getLogs();
+    }
+
+    //Requests for products using MySQL with JPA
+    @PostMapping
+    public Product create(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAll(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getOne(@PathVariable Long id) {
+        return productService.getProduct(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        productService.deleteProduct(id);
     }
 }
---
+++ src/main/java/com/statistics/statisticNonsense/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.statistics.statisticNonsense.service;
+
+import com.statistics.statisticNonsense.entity.Product;
+import com.statistics.statisticNonsense.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProduct(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        return productRepository.findById(id)
+                .map(p -> {
+                    p.setName(product.getName());
+                    p.setPrice(product.getPrice());
+                    return productRepository.save(p);
+                })
+                .orElse(null);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
