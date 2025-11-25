dde7e76b9204b0b1727f58300a8c4ccab94c1b88
jari.tenhunen <jari.tenhunen@outlook.com>
2025-11-25 09:47:00 +0100

    plugin for react is only available for the paid version so Ill just create a new project for frontend, enabled Cors in controller

--- src/main/java/com/statistics/statisticNonsense/controller/StatisticController.java
+++ src/main/java/com/statistics/statisticNonsense/controller/StatisticController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "", allowPrivateNetwork = "")
 @RestController
 @RequestMapping("/api")
 public class StatisticController {
